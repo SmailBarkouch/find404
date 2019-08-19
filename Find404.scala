0d600171c8ad6772c52b3aa0d0a922bd7d70c980
Smail Barkouch <728378@gmail.com>
2019-08-19 18:24:53 -0400

    add the functionality of printing all files in a provided directory

--- Find404.scala
+++ Find404.scala
@@ -1,5 +1,16 @@
+import java.io._
+import scala.io.Source._
+import scala.io.StdIn._
+
 object Find404 {
+    
     def main(args: Array[String]): Unit = {
-        
+        println("Provide a directory")
+        val directory = new File(readLine())
+
+        val directorySet = directory.listFiles()
+        val fileSet = directorySet.filter(_.getClass().getName().indexOfSlice(".") == -1)
+
+        fileSet.foreach(println(_))
     }
 }
