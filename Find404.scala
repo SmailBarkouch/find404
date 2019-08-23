250dc79820d981df8fdd17878277bbe8b6f1d2db
Smail Barkouch <728378@gmail.com>
2019-08-23 13:36:06 -0400

    Added more effective directory searcher, as well as more methods (though yet to have full functionality)

--- Find404.scala
+++ Find404.scala
@@ -3,18 +3,59 @@ import scala.io.Source._
 import scala.io.StdIn._
 import scala.collection.mutable.ArrayBuffer
 
-val fileSet = ArrayBuffer[File]()
-
-try {
-    val directorySet = new File(args(0))
-    exploreDirect(directorySet, fileSet)
-} catch {
-    case _: NullPointerException => println("Not a valid directory")
+var fileSet = ArrayBuffer[File]()
+var ignoreSet = ArrayBuffer[String]()
+val directorySet = new File(args(0))
+if(directorySet.exists() == false) {
+    println("This directory does not exist")
+} else {
+    fileSet = exploreDirect(directorySet)
+    ignoreSet = readFile(searchFor(fileSet, ".gitignore"))
 }
 
-def exploreDirect(directory: File, fileSet: ArrayBuffer[File]): Unit = {
+
+def exploreDirect(directory: File): ArrayBuffer[File] = {
+    var subFileSet = ArrayBuffer[File]()
     directory.listFiles().foreach {
-        System.out.println(directory.getAbsolutePath())
-        x: File => if(x.isFile) fileSet += x else exploreDirect(x, fileSet)
+        x: File => 
+        if(x.isFile()) subFileSet += x
+        else subFileSet ++= exploreDirect(x)
     }
+    subFileSet
 }
+
+def searchFor(fileSet: ArrayBuffer[File], name: String): File = {
+    var foundFile = new File("/")
+    fileSet.foreach {
+        x =>
+        if (x.getAbsolutePath.indexOf(name) != -1) {
+            foundFile = x
+        }
+    }
+    foundFile
+}
+
+def readFile(textFile: File): ArrayBuffer[String] = {
+    val textSet = ArrayBuffer[String]()
+    val readableFile = new BufferedReader(new FileReader(textFile))
+    for(i <- 0 until readableFile.lines().count().toInt) {
+        textSet += readableFile.readLine()
+    }
+    textSet
+}
+
+def filterFiles(fileSet: ArrayBuffer[File], ignoreSet: ArrayBuffer[String]): ArrayBuffer[File] = {
+    val filteredSet = fileSet.filter {
+        x =>
+        var option = true
+        for(i <- 0 until ignoreSet.length) {
+            if (x.getAbsolutePath.indexOf(ignoreSet(i)) != -1) {
+                 option = false
+            }
+        }
+        option
+    }
+    filteredSet
+}
+
+
