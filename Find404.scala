a1335b809f26bd83b5be9fd96dab5bbeb119bbbf
Smail Barkouch <728378@gmail.com>
2019-10-06 13:58:02 -0400

    changes

--- Find404.scala
+++ Find404.scala
@@ -5,21 +5,36 @@ import scala.collection.mutable.ArrayBuffer
 import sys.process._
 import java.nio.charset.StandardCharsets
 import scala.collection.JavaConverters._
+import java.net.URI
+import scala.util.matching.Regex
+import java.net.HttpURLConnection
 
 object Find404 {
   def main(args: Array[String]): Unit = {
     val a = getFileNames
       .map(new File(_))
-      .map(readFile)
-      .foreach (println)
-      
+      .map(readFile(_))
+      .map(getURLS(_))
+      .flatten
+      .map(x =>(x, getStatusCode(x)))
+      .foreach(println(_))
   }
 
   def getFileNames: Array[String] = ("git ls-files --full-name".!!).split("\n")
 
   def readFile(fileName: File): String = {
     val source = Source.fromFile(fileName)
-    val lines = try source.mkString finally source.close() //if file is too large, could run out of memory
+    val lines = try source.mkString
+    finally source.close() //if file is too large, could run out of memory
     lines
+  }
+
+  def getURLS(text: String): List[URI] = {
+    "(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]".r.findAllIn(text).map(new URI(_)).toList
+  }
+
+  def getStatusCode(url: URI): Integer = {
+    val http = url.toURL().openConnection()
+    http.asInstanceOf[HttpURLConnection].getResponseCode()
   }
 }
