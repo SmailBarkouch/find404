429f919b22e0198bd1c462c24efab1cca62bb1eb
Smail <728378@gmail.com>
2019-10-06 16:17:47 -0400

    fixed unreadable files

--- src/main/scala/find404/App.scala
+++ src/main/scala/find404/App.scala
@@ -1,42 +1,50 @@
 package find404
 
 import java.io._
+import java.net.{HttpURLConnection, URI}
+
 import scala.io._
-import java.nio.file.Files
-import scala.collection.mutable.ArrayBuffer
-import sys.process._
-import java.nio.charset.StandardCharsets
-import scala.collection.JavaConverters._
-import java.net.URI
-import scala.util.matching.Regex
-import java.net.HttpURLConnection
+import scala.sys.process._
+import scala.util.control.NonFatal
 
 object App {
   def main(args: Array[String]): Unit = {
-    val a = getFileNames
+    getFileNames
       .map(new File(_))
-      .map(readFile(_))
-      .map(getURLS(_))
-      .flatten
-      .map(x =>(x, getStatusCode(x))) // mention
+      .map(readFile)
+      .flatMap(getURLS)
+      .map(x => (x, getStatusCode(x))) // mention
       .foreach(println(_))
   }
 
-  def getFileNames: Array[String] = ("git ls-files --full-name".!!).split("\n")
+  def getFileNames: Array[String] = "git ls-files --full-name".!!.split("\n")
 
   def readFile(fileName: File): String = {
+    println(fileName.getAbsolutePath)
     val source = Source.fromFile(fileName)
-    val lines = try source.mkString
-    finally source.close() //if file is too large, could run out of memory
+    val lines = {
+      try {
+        source.mkString
+      } catch {
+        case NonFatal(_) => ""
+      } finally {
+        source.close()
+      }
+    }
+
+    //if file is too large, could run out of memory
     lines
   }
 
   def getURLS(text: String): List[URI] = {
-    "(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]".r.findAllIn(text).map(new URI(_)).toList
+    "(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]".r
+      .findAllIn(text)
+      .map(new URI(_))
+      .toList
   }
 
   def getStatusCode(url: URI): Integer = {
-    val http = url.toURL().openConnection()
+    val http = url.toURL.openConnection()
     http.asInstanceOf[HttpURLConnection].getResponseCode()
   }
 }
